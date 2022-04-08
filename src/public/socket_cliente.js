24e71f9d82c86e90a341caeb449b894042903863
juacko <juacko18@gmail.com>
2022-04-08 18:27:35 -0500

    avance juacko

--- src/public/socket_cliente.js
+++ src/public/socket_cliente.js
@@ -1,29 +1,31 @@
-const socket=io.connect()
+const socket = io.connect();
 
-export const guardarDatosActa = ()=>{socket.emmit("nuevaActaVerificacion",{
-actaVerificacion,
-fecha,
-hora,
-lugarVerificacion,
-actaInmovilizacion,
-actaIncautacion,
-numeroInmovilizacion,
-expediente,
-solicitante,
-otros,
-nameConcurrente1,
-DNIConcurrente1,
-calidadConcurrente1,
-nameConcurrente2,
-DNIConcurrente2,
-calidadConcurrente2,
-nameConcurrente3,
-DNIConcurrente3,
-calidadConcurrente3,
-descripcion,
-fotografia,
-cantidadMuestra,
-muestra
-})}
-export const actualizarDatosActa=""
-export const obtenerDatosActa=""
+export const guardarDatosActa = () => {
+  socket.emmit("nuevaActaVerificacion", {
+    actaVerificacion,
+    fecha,
+    hora,
+    lugarVerificacion,
+    actaInmovilizacion,
+    actaIncautacion,
+    numeroInmovilizacion,
+    expediente,
+    solicitante,
+    otros,
+    nameConcurrente1,
+    DNIConcurrente1,
+    calidadConcurrente1,
+    nameConcurrente2,
+    DNIConcurrente2,
+    calidadConcurrente2,
+    nameConcurrente3,
+    DNIConcurrente3,
+    calidadConcurrente3,
+    descripcion,
+    fotografia,
+    cantidadMuestra,
+    muestra,
+  });
+};
+export const actualizarDatosActa = "";
+export const obtenerDatosActa = "";
